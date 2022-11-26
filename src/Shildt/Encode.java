1d9a16d884bdf414c3864b2e82eae0142d84e8c2
Sait75 <saa4981@gmail.com>
2022-11-26 11:31:09 +0300

    commit

--- src/Shildt/Encode.java
+++ src/Shildt/Encode.java
@@ -5,20 +5,21 @@ public class Encode {
 
     public static void main(String[]args){
         String msg = "Шифруем этот текст";
-        String codMsg = "";
-        String encMsg = "";
+        StringBuilder codMsg = new StringBuilder();
+        StringBuilder encMsg = new StringBuilder();
         int key = 88; // ключ шифрования
 
         System.out.println("Исходное сообщение:" + msg);
 
         for (int i=0; i<msg.length(); i++){
-            codMsg = codMsg + (char) (msg.charAt(i)^key);
+            codMsg.append((char) (msg.charAt(i) ^ key));
         }
         System.out.println("Зашифрованное сообщение: " + codMsg);
 
         for (int i = 0; i<codMsg.length(); i++){
-            encMsg = encMsg + (char) (codMsg.charAt(i)^key);
+            encMsg.append((char) (codMsg.charAt(i) ^ key));
         }
         System.out.println("Дешифрованное сообщение: " + encMsg);
+        System.out.println("sdfs");
     }
 }
